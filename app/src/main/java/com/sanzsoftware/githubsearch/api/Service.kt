70ba53e76af1086832e679d3bc8cd530238003fc
AlvaroSanzRodrigoq <copacabanasanz@gmail.com>
2020-12-29 20:38:45 +0100

    Basic Service singleton for retrofit2 implementation

--- app/src/main/java/com/sanzsoftware/githubsearch/api/Service.kt
+++ app/src/main/java/com/sanzsoftware/githubsearch/api/Service.kt
@@ -1,4 +1,26 @@
 package com.sanzsoftware.githubsearch.api
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class Service {
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.github.com/search/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    companion object{
+
+        @Volatile private var instance: Service? = null
+
+        fun getInstance() =
+            instance ?: synchronized(this) {
+                instance ?: Service().also {
+                    instance = it
+                }
+            }
+
+    }
 }
