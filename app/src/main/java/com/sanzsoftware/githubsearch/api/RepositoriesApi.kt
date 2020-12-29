bb12853c434655432a9af616ec9ecaae6f0f9c73
AlvaroSanzRodrigoq <copacabanasanz@gmail.com>
2020-12-29 23:32:33 +0100

    Created RepositoriesApi.kt interface

--- app/src/main/java/com/sanzsoftware/githubsearch/api/RepositoriesApi.kt
+++ app/src/main/java/com/sanzsoftware/githubsearch/api/RepositoriesApi.kt
@@ -1,4 +1,9 @@
 package com.sanzsoftware.githubsearch.api
 
+import retrofit2.Call
+import retrofit2.http.Query
+import retrofit2.http.Url
+
 interface RepositoriesApi {
+    fun getRepositories(@Url url:String, @Query("q") search: String?): Call<RepositoryResponse>
 }
